7a16fab23ff038477e84c14b21c48bd5806a60cc
Olivia Toolson <livytoolson@gmail.com>
2020-10-29 08:36:30 -0700

    added test file and made skeleton of test

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,23 @@
+
+
+import React from 'react';
+import { render, scree } from '@testing-library/react';
+import ContactForm from './ContactForm';
+
+test('ContactForm adds new contacts to the list', () => {
+    // type into all inputs
+    // 1. query for all inputs
+    // 2. run the change event to add text
+
+
+
+    // click submit button
+    // 1. query for the button
+    // 2. run the click event on the button
+
+
+    
+    // assert that any new contact is in the list
+    // 1. query for the new contact text
+    // 2. assert that it is being rendered
+})
