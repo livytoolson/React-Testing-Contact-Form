0aea2ab6ef7b047cacf9171f2c95a78d3452b1ff
Olivia Toolson <livytoolson@gmail.com>
2020-10-29 08:46:45 -0700

    tested that input labels were being rendered

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,13 +1,18 @@
 
 
 import React from 'react';
-import { render, scree } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import ContactForm from './ContactForm';
 
 test('ContactForm adds new contacts to the list', () => {
+    render(<ContactForm />);
     // type into all inputs
     // 1. query for all inputs
     // 2. run the change event to add text
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const lastNameInput = screen.getByLabelText(/last name/i);
+    const emailNameInput = screen.getByLabelText(/email/i);
+    const messageNameInput = screen.getByLabelText(/message/i);
 
 
 
@@ -16,7 +21,7 @@ test('ContactForm adds new contacts to the list', () => {
     // 2. run the click event on the button
 
 
-    
+
     // assert that any new contact is in the list
     // 1. query for the new contact text
     // 2. assert that it is being rendered
